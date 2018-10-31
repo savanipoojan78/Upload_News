80f5f7d00e646889e1252fcb25e4688c56debaa2
savanipoojan78 <savanipoojan78@gmail.com>
2018-10-31 22:59:06 +0530

    Some Changes There is error Something need to fix

--- app/src/main/java/com/example/poojan/upload_news/MainActivity.java
+++ app/src/main/java/com/example/poojan/upload_news/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.poojan.upload_news;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,20 +15,50 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private static final int RC_PHOTO_PICKER = 2;
     DatabaseReference databaseReference;
     FirebaseDatabase firebaseDatabase;
+    private Uri filePath;
     news News;
     String publishdate;
+    FirebaseStorage storage;
+    StorageReference storageReference;
     long i;
     EditText section,date,title,trailtext,weburl,author,time;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
         News=new news();
         firebaseDatabase=FirebaseDatabase.getInstance();
         databaseReference=firebaseDatabase.getReference("response");
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
         initial();
 
         submit.setOnClickListener(new View.OnClickListener() {
@@ -48,13 +85,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                // increase();
                 initial();
-                Log.e("Child after Intial:----",Long.toString(i));
+                uploadImage();
+//                Log.e("Calling The uploadImage",News.getThumbnail());
+               // Log.e("Child after Intial:----",Long.toString(i));
                 i++;
                 Log.e("Child after i++ Intial:",Long.toString(i));
                 getvalues();
                 String s=Long.toString(i);
                 databaseReference.child(s).setValue(News);
-                Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_SHORT).show();
+
+                //Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_SHORT).show();
 
 
             }
@@ -85,22 +125,6 @@ public class MainActivity extends AppCompatActivity {
         Log.e("PublishDate::------",publishdate);
 
     }
-//    private void increase()
-//    {
-//        databaseReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                i=dataSnapshot.getChildrenCount();
-//                Log.e("Child:----",Long.toString(i));
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
-//    }
     private void initial()
     {
         databaseReference.addValueEventListener(new ValueEventListener() {
@@ -116,4 +140,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void uploadImage() {
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+           final StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            progressDialog.dismiss();
+                            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String downloadUrl=uri.toString();
+                                    setthumbnail(downloadUrl);
+                                  //  Log.e("Download URI:------",News.getThumbnail());
+                                }
+                            });
+                            Toast.makeText(MainActivity.this, "Data Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+
+    }
+    private void setthumbnail(String s)
+    {
+        String f=s;
+
+    }
+
 }
